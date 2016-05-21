5ce5653be3eb3f5d4069c329d73ea50c7ebdda73
bgcc93 <brunoo_o@hotmail.com>
2016-05-21 02:12:00 -0300

    Exercicio 5 - Simpson Rule

--- LinkedList/LinkedList/Calculations.h
+++ LinkedList/LinkedList/Calculations.h
@@ -1,3 +1,4 @@
+#define _USE_MATH_DEFINES
 #include <iostream>
 #include "LinkedList.h"
 #include <cmath>
@@ -18,6 +19,7 @@ public:
 	double stdDeviationX;
 	double stdDeviationY;
 	double correlationCoeficient;
+	double functionArea;
 
 	Calculations(){
 		xAvg = 0;
@@ -32,6 +34,7 @@ public:
 		stdDeviationX = 0;
 		stdDeviationY = 0;
 		correlationCoeficient = 0;
+		functionArea = 0;
 	}
 	Calculations(LinkedList& l){
 		for ( int i = 0; i < l.Length(); i++){
@@ -44,12 +47,14 @@ public:
 			xySum += (l[i]->X() * l[i]->Y());
 		}
 	}
-
 	void Mean(LinkedList& l, bool ln){
 		for (int i = 0; i < l.Length(); i++){
 			if (ln){
 				xSum += log(l[i]->X());
 				ySum += log(l[i]->Y());
+			}else{
+				xSum += l[i]->X();
+				ySum += l[i]->Y();
 			}
 		}
 		xAvg = xSum / l.Length();
@@ -85,16 +90,13 @@ public:
 		b1 = ( xySum - ( l.Length() * xAvg * yAvg ) ) / ( xSqrSum - ( l.Length() * xAvg * xAvg ) );
 		b0 = yAvg - b1 * xAvg;
 	}	
-	
 	double Correlation(LinkedList& l){
 		correlationCoeficient = (l.Length() * xySum - xSum * ySum) / sqrt( (l.Length() * xSqrSum - xSum * xSum ) * ( l.Length() * ySqrSum - ySum * ySum ) );
 		return correlationCoeficient;
 	}
-
 	double PredictNext(double x){
 		return b0 + b1 * x;
 	}
-
 	void CalculateRanges(){
 		double lVS = xAvg - 2 * stdDeviationX;
 		double lS  = xAvg - stdDeviationX;
@@ -110,5 +112,48 @@ public:
 		std::cout << "Very Big   :" << pow(euler,lVL) << "\n";
 		std::cout << "===========================\n";
 	}
+	static double Gamma(double x){
+		if ( x - 1 < 0){
+			return sqrt(M_PI)/2;
+		}
+		else if( x - 1 == 0){
+			return 1;
+		}else{
+			return ( x * Gamma(x-1) );
+		}
+	}
+	static double  IntegrateDistributionT(double x, double dof){
+		return ( Gamma( ( (double)(dof+1) / 2 ) -1 ) / ( pow( ((double)dof * M_PI), .5) * Gamma( ((double)dof/2) - 1 ) ) ) * pow((1 + (x*x)/dof), -1 * (double)(dof+1)/2);
+	}
+	double CalculateFunctionArea(double xStart, double xEnd, int dof, int segs, double error, double(*f)(double,double) ){
+		double g = 0;
+		double totalX = xEnd - xStart;
+		double w = totalX / segs;
+		double w2 = totalX / (segs * 2);
+		double area = 0;
 
+		area += f(xStart, dof);
+		area += f(xEnd, dof);
+
+		functionArea += area;
+
+		for(int i = 1 ; i < segs; i ++){
+			if ( i % 2 == 0 ){area += f( ( totalX / segs * i ), dof) * 2;}
+			else{area += f( ( totalX / segs * i ), dof) * 4;}				
+		}
+
+		for(int i = 1 ; i < segs * 2; i ++){
+			if ( i % 2 == 0 ){functionArea += f( ( totalX / (segs * 2 ) * i ), dof) * 2;}
+			else{functionArea += f( ( totalX / (segs * 2) * i) , dof) * 4;}				
+		}
+		
+		area *= w/3;
+		functionArea *= w2/3;
+
+		if ( (functionArea - area) > error) {
+			functionArea = CalculateFunctionArea(xStart, xEnd, dof, segs*2, error, f);
+		}
+
+		return functionArea;
+	}
 };
--- LinkedList/LinkedList/main.cpp
+++ LinkedList/LinkedList/main.cpp
@@ -1,35 +1,9 @@
 #include "Calculations.h"
 
 int main (int argc, char *argv[] ) { 
-	//Calculations::MeanAndDeviation();
-	LinkedList l;
 	Calculations c;
-	std::cout << "Insert a number or x to start the calculations\n";
-	do{
-		std::cin.clear();
-		double val = 0;
-		std::cout << "Number :";
-		if ( !(std::cin >> val) ){
-			break;
-		}
-		l.CreateNode(val);
-	}while(true);
-
-	c.Mean(l, true);
-	c.StdDeviation(l, true);
-	c.CalculateRanges();
-
+	//std::cout << "\n\n" << c.CalculateFunctionArea(0,1.1,9,10,0.00001,Calculations::IntegrateDistributionT) << "\n\n";
+	//std::cout << "\n\n" << c.CalculateFunctionArea(0,1.182,10,10,0.00001,Calculations::IntegrateDistributionT) << "\n\n";
+	std::cout << "\n\n" << c.CalculateFunctionArea(0,2.75,30,10,0.00001,Calculations::IntegrateDistributionT) << "\n\n";
 	system("pause");
-
-	/*
-		l.CreateNode(591);
-		l.CreateNode(114);
-		l.CreateNode(229);
-		l.CreateNode(230);
-		l.CreateNode(270);
-		l.CreateNode(128);
-		l.CreateNode(1657);
-		l.CreateNode(624);
-		l.CreateNode(1503);
-		*/
 }	
